cddd0727aa1c31db2f273958e92255439795c854
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-04 17:48:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,17 +3,24 @@
 katz_deli = [] #empty array
 
 
-def line
-if 
-else puts  "The line is currently empty."
-end
+def line(katz_deli)
+  if katz_deli.empty?
+      puts "The line is currently empty."
+    else
+      current_line = "The line is currently:"
+      katz_deli.each.with_index(1) do |person, i|
+        current_line << " #{i}. #{person}"
+      end
+      puts current_line
+    end
+  end
 
-count = 0 
+count = 0
 def take_a_number(katz_deli, name)
   4.times do
   puts "Hello #{name}, you are number #{count} in line"
   count = count+1 
-  
+
 end
 
 def now_serving
