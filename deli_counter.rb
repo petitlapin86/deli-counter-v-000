57443650e9840d46e38c59fcdbfd95f6d1991b21
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-04 17:44:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,8 +8,12 @@ if
 else puts  "The line is currently empty."
 end
 
-
+count = 0 
 def take_a_number(katz_deli, name)
+  4.times do
+  puts "Hello #{name}, you are number #{count} in line"
+  count = count+1 
+  
 end
 
 def now_serving
